ee657edb9c664b75c7e7cc3ceda32e863d023ad6
yes551 <141025070+yes551@users.noreply.github.com>
2024-05-20 07:05:09 +0330

    Update manager.py
    create admin function

--- manager.py
+++ manager.py
@@ -1,1 +1,15 @@
 
+import argparse
+import os
+import shutil
+
+def create_admin(username, password):
+    admin_file_path = os.path.join('APelahishokr', 'admin.txt')
+    if os.path.exists(admin_file_path):
+        print("Error: Admin already exists.")
+        return
+
+    os.makedirs(os.path.dirname(admin_file_path), exist_ok=True)
+    with open(admin_file_path, 'w') as file:
+        file.write(f'Username: {username}\nPassword: {password}')
+    print("Admin created successfully.")
